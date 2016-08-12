abcaea313c6802dcb5d5a07f6eb8f6c5284c0150
JustRamon <justramon@outlook.com>
2016-08-12 15:00:37 +0200

    Thing.

--- src/main/java/me/justramon/mcsrvgrab/core/Launch.java
+++ src/main/java/me/justramon/mcsrvgrab/core/Launch.java
@@ -4,12 +4,12 @@ package me.justramon.mcsrvgrab.core;
 import javax.swing.*;
 
 /**
- * Created by ramon on 8/11/16.
+ * Created by JustRamon on 8/11/16.
  */
-public class Launch{
+public class Launch {
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable(){
-			@Override
+
 			public void run() {
 				new MainGUI().setVisible(true);
 			}
